bd38b4a907130e5673ed1b0f03f2cefaa6b8174a
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-11-03 04:55:53 +0800

    fixing all

--- controller/AppRequest.js
+++ controller/AppRequest.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 
 const BloodLettingActivityModel = require('../model/ActivityModel');
 const DonorModel = require('../model/DonorModel');
+const healthCenterModel = require('../model/HealthCenter');
+const PatientModel = require('../model/Patient');
 const PatientRequestModel = require('../model/PatientRequest');
+const PhysicianModel = require('../model/Physician');
 
 
 router.get('/getRequestById/:id',async (req,res)=>{
@@ -28,6 +31,36 @@ router.get('/getRequestById/:id',async (req,res)=>{
 router.get('/getAllDonors',async(req,res)=>{
   try {
       const activities = await DonorModel.find({});
+      if(activities){
+          res.status(200).json(activities);
+      }
+  } catch (error) {
+      console.log(error)
+  }
+})
+router.get('/getAllPatient',async(req,res)=>{
+  try {
+      const activities = await PatientModel.find({});
+      if(activities){
+          res.status(200).json(activities);
+      }
+  } catch (error) {
+      console.log(error)
+  }
+})
+router.get('/getAllPhysician',async(req,res)=>{
+  try {
+      const activities = await PhysicianModel.find({});
+      if(activities){
+          res.status(200).json(activities);
+      }
+  } catch (error) {
+      console.log(error)
+  }
+})
+router.get('/getAllCenters',async(req,res)=>{
+  try {
+      const activities = await healthCenterModel.find({});
       if(activities){
           res.status(200).json(activities);
       }
