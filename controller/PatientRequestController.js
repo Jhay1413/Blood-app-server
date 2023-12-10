939fdb91e12845aaef3ee6ec36c5cd0f10307d1e
Jhon <jhonchristian.evsu.edu.ph>
2023-12-11 06:39:49 +0800

    change id

--- controller/PatientRequestController.js
+++ controller/PatientRequestController.js
@@ -5,7 +5,7 @@ const Request = require('../model/PatientRequest')
 router.get('/getRequestById/:id',async (req,res)=>{
     try {
         const id = req.params.id
-      const allRequestData = await Request.findById(id);
+      const allRequestData = await Request.findById({requestId:id});
       res.status(200).json(allRequestData)
     } catch (error) {
       console.log(error)
